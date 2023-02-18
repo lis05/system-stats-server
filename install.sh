264e1f1ec472b5393e54941c00f0584ee4cdd375
Igor Lukyanov <lis05st@gmail.com>
2023-02-18 18:50:33 +0200

    system-stats-server

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 -m pip install psutil
+python3 -m pip install pid
 
 python3 replace-tokens.py system-stats-server.service
 sudo cp system-stats-server.service.correct /etc/systemd/system/system-stats-server.service
